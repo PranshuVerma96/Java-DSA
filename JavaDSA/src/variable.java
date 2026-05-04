187389a5ac90ff7e908243cfa2c4ed0ecea96029
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-05-04 10:56:03 +0530

    Add files via upload
    variables are stored the named of memory location where data are stored.

---
+++ JavaDSA/src/variable.java
@@ -0,0 +1,24 @@
+static void main(){
+    int age = 30;
+    System.out.println(age);
+
+    int mark1 = 20;
+    int mark2 = 23;
+     int height2 = 10;
+
+//     No resvered keyword
+//    static int  = 4
+
+    int main  = 3;
+    System.out.println(main);
+
+//    variable name is meaning full name
+
+    int Day_in_Year = 365;
+
+//    camelCase
+    int totalMarksSum = 300;
+    System.out.println(totalMarksSum);
+
+    System.out.println(mark1 + mark2);
+}
