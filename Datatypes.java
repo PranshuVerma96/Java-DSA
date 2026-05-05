607aedf42def0fabfa0117185dc58fa5d3e93d21
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-05-05 10:06:14 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,46 @@
+public class Datatypes {
+    static  void  main() {
+        //create // numeric - num
+
+//        byte num1 = 500;
+
+//        short num2 = 50000;
+//        System.out.println(num2);
+//        System.out.println(num2);
+
+        long num4 = 3434343;
+
+
+        int age;
+        age = 90;
+        System.out.println(age);
+
+
+//        floating data
+        float num5 = 3.4f;
+        System.out.println(num5);
+
+        double num6 = 1.232242254634;
+        System.out.println(num6);
+
+//        other char
+
+        boolean elibableToBoat = true;
+        System.out.println(elibableToBoat);
+
+        char firstCharector  = 'A';
+        System.out.println(firstCharector);
+
+//        ascii value
+//        type conversion
+        char data = 'a';
+        System.out.println((char)(data + 3));
+
+//        type casting
+//        jab me chote data type co daluga in the bade datag[e ke ander
+
+        long value = 12345222;
+        int value2 = (int)(value);
+        System.out.println(value2);
+    }
+}
