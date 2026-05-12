48e0487302969978509c1d5896d53bb04ee7cb44
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-05-12 20:39:27 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,24 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
+
+/* public class ArraySum {
+    public static void main(String[] args) {
+
+        int[] arr = {5, 10, 15, 20};
+        int sum = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+} */
