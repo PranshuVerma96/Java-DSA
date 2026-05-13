327809d575d5d3df6ed24042603a9b552ff1cc86
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-05-13 20:57:17 +0530

    Create arraypractice.java

---
+++ arraypractice.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
+/*public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {5, 8, 2, 10, 3};
+
+        int max = arr[0];
+
+        for(int i = 1; i < arr.length; i++) {
+
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Maximum = " + max);
+    }
+}*/
