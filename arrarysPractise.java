64081f44bd83fcc258bf7fc9cda9ad5b4d4ba71e
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-05-16 10:50:44 +0530

    Add files via upload
    this is all about array creation and traversal.

---
+++ arrarysPractise.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class arrarysPractise {
+    static void main() {
+//        declearation
+      //  int arr [];
+//        allocation
+//        arr = new int[5];
+
+//        initilization
+        int brr [] = {2 , 4, 5};
+      //  System.out.println("value at 0 index ==> " + brr[0]);
+       // System.out.println("value at 1 index ==> " + brr[1]);
+
+      //  System.out.println("value at 2 index ==> " + brr[2]);
+
+//        for(int index= 0; index<=brr.length-1; index++){
+//            //System.out.println(brr[index]);
+//        }
+//
+////        for each
+//        for(int value : brr){
+//           // System.out.println(value);
+//        }
+////        taking input in array
+//
+        Scanner sc = new Scanner(System.in);
+//       // System.out.println("Enter your arrays value : ");
+//        int arr [] = new int[5];
+//        int n = arr.length;
+////        for input
+//        for(int i= 0 ; i<n; i++){
+//            arr[i] = sc.nextInt();
+//        }
+//
+////        for printing
+//        for(int i = 0 ; i<n; i++){
+//           // System.out.println("value is ==> " + arr[i]);
+//        }
+
+//        task 1
+//        int arr[] = {1,4,5,6,7}; // print all value of array
+//        int n = arr.length;
+//        for (int i=0; i< n; i++){
+//            System.out.println(arr[i]);
+//
+//        }
+
+//        task two taking input of array and sum of those element
+
+//        int arr[] = new int[9];
+//        int n = arr.length;
+//      for (int i =0; i<n; i++){
+//          System.out.println("Enter the value of index " + i);
+//          arr[i]= sc.nextInt();
+
+//      }
+//      int sum = 0;
+//        System.out.println("the element of array is :" );
+//        for(int i = 0; i<n; i++){
+//            System.out.println(arr[i]);
+//            sum += arr[i];
+//
+//        }
+//        System.out.println("the sum of all Elements " + sum);
+
+    }
+}
