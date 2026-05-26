8b529cf65897f474fe8004cb488f94878e473d25
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-05-26 12:14:54 +0530

    Add files via upload

---
+++ Transport/Main.java
@@ -0,0 +1,15 @@
+package Transport;
+
+public class Main {
+    public static void main(String[] args) {
+//        Car c = new Car("Maruti","500",4,"4","automatic");
+//        c.startEngine();
+//        c.startAc();
+//        c.startEngine();
+
+        MoterCycle m = new MoterCycle("Splender" , "xline",2,"Stylish","Colle");
+        m.startEngine();
+        m.wheelie();
+        m.stopEngine();
+    }
+}
