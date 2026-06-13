74cf7a0836acd4cd8d57f0351b86e2c890715f30
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-06-13 16:34:18 +0530

    Add files via upload
    there are some extra information about set.

---
+++ JavaCollectionFrameWork/Student.java
@@ -0,0 +1,30 @@
+import java.util.Objects;
+
+public class Student {
+    public  int rolNo;
+    public String name;
+
+    public Student(int rolNo , String name){
+        this.rolNo = rolNo;
+        this.name= name;
+    }
+
+    public String toString(){
+        return "Student{" +
+                "rollNo=" + rolNo +
+                " , name = '" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+}
