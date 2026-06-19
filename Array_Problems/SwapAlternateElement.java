35905919409af64e25cb6afc42ea7f920e953793
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-06-19 22:28:47 +0530

    Add files via upload

---
+++ Array_Problems/SwapAlternateElement.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class SwapAlternateElement {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+
+        for (int i = 0; i < arr.length - 1; i += 2) {
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
