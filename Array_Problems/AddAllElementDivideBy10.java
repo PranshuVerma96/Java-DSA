0785f43a24cc924c96def31eb4481a146c96045b
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-06-19 22:29:35 +0530

    Add files via upload

---
+++ Array_Problems/AddAllElementDivideBy10.java
@@ -0,0 +1,19 @@
+public class AddAllElementDivideBy10 {
+    public static void main(String[] args) {
+        int mul = 1;
+        int arr [] = {1,2,3,4,5,6,7};
+        for(int i =0; i<=arr.length; i++){
+            mul*= i;
+        }
+
+//        print all element
+        System.out.println();
+        for (int i : arr){
+            System.out.print(i + " ");
+        }
+
+    int division = mul /10;
+        System.out.println(division);
+
+    }
+}
