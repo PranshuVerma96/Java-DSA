9366500e216edfeec35b03f18f7e4c4e9ba9ea58
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-06-20 11:17:26 +0530

    Add files via upload

---
+++ ArrayManupulationProblem/ReverseArrayElement.java
@@ -0,0 +1,31 @@
+public class ReverseArrayElement {
+    static void reverseArray(int arr[]){
+        int start = 0;
+        int end = arr.length - 1;
+
+        System.out.println("Orignal array ");
+        for(int i : arr){
+            System.out.print(i + " ");
+        }
+        while (start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start ++;
+            end --;
+        }
+        System.out.println();
+        System.out.println("the Reverse array");
+        for(int i : arr){
+            System.out.print(i  + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8};
+        reverseArray(arr);
+    }
+}
+
+//time complextiy = o(n)
