8b3d7ff2b7b5c0aa4c80201576a66bc453085f20
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-06-25 09:49:28 +0530

    Implement palindrome check for an integer

--- LeetCode_Problems_Soulations/PalindromeNumber.java
+++ LeetCode_Problems_Soulations/PalindromeNumber.java
@@ -1,1 +1,29 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+    
+    if(x < 0){
+        return false;
+    }
 
+   int original = x;
+    int reverseNum = 0;
+    while(x >0){
+        // step 1 find the first last digit
+        int lastDigit = x % 10;
+
+        // add in the reverse num 
+        reverseNum = reverseNum*10 + lastDigit;
+
+        // revmove the last digit
+        x = x/10;
+    }
+    if(original == reverseNum){
+        return true;
+    }
+    else{
+        return false;
+    }
+        
+    
+    }
+}
