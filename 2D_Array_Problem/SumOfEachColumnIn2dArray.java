2fc6cb885cbd211ee1ae5b5222c1153a88a87cf2
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-06-26 10:14:29 +0530

    Add files via upload

---
+++ 2D_Array_Problem/SumOfEachColumnIn2dArray.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SumOfEachColumnIn2dArray {
+    static List<Integer> columnSums(int arr[] []){
+        List<Integer> result = new ArrayList<>();
+
+        int n = arr.length;
+        int m = arr[0].length;
+
+        for(int col =0; col<m; col++){
+            int sum = 0;
+            for (int row= 0; row<n; row++){
+                int value = arr[col][row];
+
+                sum = sum +value;
+            }
+            result.add(sum);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int arr[][] = { {1,2,3},{3,4,6},{5,6,7}};
+
+        List<Integer> result = columnSums(arr);
+
+        System.out.println(result);
+
+
+    }
+}
