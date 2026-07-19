9d5582fc119e33a38e905d7daa0f59968ec600be
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-07-19 11:19:48 +0530

    Implement search in 2D array without sorting

---
+++ 2D_Array_Problem/SearchIn2dWhitoutSortedArray.java
@@ -0,0 +1,29 @@
+public class SearchIn2dWithoutSortedElement {
+    static boolean searchIn2DWithoutSortElemnt(int arr[][],int target){
+        int totalRow = arr.length;
+        int totalCol = arr[0].length;
+
+        int row =0;
+        int col = totalCol - 1;
+
+        while(row < totalRow && col>=0){
+            if(arr[row][col]==target){
+                return true;
+
+            } else if (arr[row][col] > target) {
+                col--; // move to left
+
+            }else{
+                // arr[row][col] <target // move to bottom
+                row++;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int arr[][] = {{1,4,7,11,15},{2,5,8,12,19},{10,13,14,17,24}};
+        int target = 5;
+        System.out.println( searchIn2DWithoutSortElemnt(arr,target));
+    }
+}
