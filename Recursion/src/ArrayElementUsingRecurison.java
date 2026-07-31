ef0d59489f915fadde5cad108ea275bd2438c455
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-07-31 11:06:51 +0530

    recursion problem task
    Today I started to learn recursion and I solved the some problem related to this.

---
+++ Recursion/src/ArrayElementUsingRecurison.java
@@ -0,0 +1,32 @@
+public class ArrayElementUsingRecurison {
+    static  void printArrayElement(int arr[] , int index){
+        // base case
+        if(index == arr.length){
+            return;
+        }
+        System.out.print(arr[index] + " ");
+        // recursive call
+        printArrayElement(arr, index+1);
+    }
+
+    // print array element in right to left
+
+    static void printArrayRightToLeft(int arr[], int index){
+        // base case
+        if(index == -1){
+            return;
+        }
+        System.out.print(arr[index] + " ");
+
+        // recrence releation
+       printArrayRightToLeft(arr,index-1);
+
+    }
+    public static void main(String[] args) {
+        int arr[] = {10,20, 30, 40, 50, 60};
+        printArrayElement(arr,0);
+        System.out.println();
+        printArrayRightToLeft(arr,arr.length-1);
+    }
+
+}
