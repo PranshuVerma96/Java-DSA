3886e055d87e29a0417dad0015f2d86a9323821b
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-09-04 11:29:11 +0530

    Add SinglyCircularLinkedList class with operations
    Implemented a Singly Circular Linked List with basic operations.

---
+++ Linked List/src/SinglyCircularLinkedList.java
@@ -0,0 +1,42 @@
+public class SinglyCircularLinkedList {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+ 
+
+
+    public static void main(String[] args) {
+        SinglyCircularLinkedList myList = new SinglyCircularLinkedList();
+
+        myList.insertAtHead(10);
+        myList.insertAtHead(20);
+
+        myList.insertAtHead(30);
+        myList.insertAtTail(40);
+
+        myList.insertAtPosition(3,50);
+
+        myList.deletionAtHead();
+        myList.deleteTail();
+
+        myList.deleteAtPosition(2);
+
+        myList.printList();
+//        System.out.println(myList.search(40));
+    }
+
+}
+
+
