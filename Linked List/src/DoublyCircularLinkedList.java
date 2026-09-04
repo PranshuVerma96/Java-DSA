489e080f2c253241282c85f4c88aa2f02f688dfe
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-09-04 11:41:53 +0530

    Add DoublyCircularLinkedList implementation
    Implement DoublyCircularLinkedList class with Node inner class.

--- Linked List/src/DoublyCircularLinkedList.java
+++ Linked List/src/DoublyCircularLinkedList.java
@@ -1,1 +1,23 @@
 
+public class DoublyCircularLinkedList {
+    static class Node{
+        int data;
+        Node prev;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size = 0;
+
+    public DoublyCircularLinkedList(){
+        head = null;
+        tail = null;
+        size = 0;
+    }
