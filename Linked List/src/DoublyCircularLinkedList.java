10e0a87a64861c74ebf5413731dfcc3ed1bff901
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-09-08 11:56:16 +0530

    Update DoublyCircularLinkedList.java

--- Linked List/src/DoublyCircularLinkedList.java
+++ Linked List/src/DoublyCircularLinkedList.java
@@ -179,7 +179,7 @@ public class DoublyCircularLinkedList {
     }
 
     //===============================
-    // Deletion
+    //Deletion
     //===============================
 
     // Deletion At head
